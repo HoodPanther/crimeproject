3d731f171664156bbacc0bbf94c467fa58cad3ca
Siyuan Xiang <sx550@nyu.edu>
2017-04-23 22:35:16 -0400

    Add lat_lon_to_zip

---
+++ src/sample_lat_lon.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+from pyspark import SparkContext
+from csv import reader
+
+SAMPLE_NUM = 100
+
+if __name__ == "__main__":
+    sc = SparkContext()
+    lines = sc.textFile(sys.argv[1], 1)
+    lines = lines.mapPartitions(lambda x: reader(x)) \
+                 .filter(lambda x: x[0] != 'CMPLNT_NUM') \
+                 .filter(lambda x: x[21]) \
+
+    results = lines.map(lambda x: ','.join([x[21], x[22]])) \
+                   .takeSample(True, num=SAMPLE_NUM, seed=1) \
+
+    sc.parallelize(results).saveAsTextFile('sample_lat_lon.out')
+
+    #results.saveAsTextFile('sample_lat_lon.out')
+
+    sc.stop()
+
