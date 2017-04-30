1bfc6df622a2165b55fc7053089b50694bf0b1ce
Siyuan Xiang <sx550@nyu.edu>
2017-04-30 17:02:28 -0400

    Minor fix

--- src/submit.sh
+++ src/submit.sh
@@ -4,6 +4,6 @@
 # save the output as:           name.out
 # run this script in src as:    bash submit.sh name
 /usr/bin/hadoop fs -rm -r "$1.out" 
-spark-submit "$1.py" crime.csv
+spark-submit "$1.py" crime_zip.csv
 /usr/bin/hadoop fs -getmerge "$1.out" ../results/"$1.out"
 head -n 100 ../results/"$1.out"
